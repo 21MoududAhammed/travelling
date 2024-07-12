6d2846df2275b7719a0e067c4a1dc07210a9f04c
21MoududAhammed <21moudud@gmail.com>
2024-07-12 12:27:48 +0600

    heading and date display have done

--- js/observe.js
+++ js/observe.js
@@ -1,20 +1,77 @@
-const targetElement = document.getElementById('simple-list-item-3');
-console.log(targetElement);
-console.log('test clicked');
+const displayContainer = document.getElementById("section-heading-display");
+const displayDateContainer = document.getElementById('section-date-display');
 
-   
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            const { top } = entry.boundingClientRect;
-            const viewportTop = window.innerHeight || document.documentElement.clientHeight;
-            if (top <= 200 && top >= -entry.boundingClientRect.height) {
-                console.log('Element is at the top of the display.');
-                targetElement.style.backgroundColor = 'lightgreen'; // Change color when at the top
-            } else {
-                targetElement.style.backgroundColor = 'lightcoral'; // Revert color when not at the top
-            }
-        });
-    }, { rootMargin: `-${50}px 0px 0px 0px`, threshold: [1.0] });
 
-    observer.observe(targetElement);
+const targetElementOne = document.getElementById("item-one-heading");
+
+// observer one
+const observerOne = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const { top } = entry.boundingClientRect;
+      const viewportTop =
+        window.innerHeight || document.documentElement.clientHeight;
+      if (top <= 200 && top >= -entry.boundingClientRect.height) {
+        const content = targetElementOne.innerText;
+        displayContainer.innerText = '';
+        displayContainer.innerText = content;
+      }
+    });
+  },
+  { rootMargin: `-${50}px 0px 0px 0px`, threshold: [1.0] }
+);
+
+observerOne.observe(targetElementOne);
+
+// observer 2
+
+const targetElementTwo = document.getElementById("item-two-heading");
+const itemTwoDate = document.getElementById('item-two-date');
+
+const observerTwo = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const { top } = entry.boundingClientRect;
+      const viewportTop =
+        window.innerHeight || document.documentElement.clientHeight;
+      if (top <= 200 && top >= -entry.boundingClientRect.height) {
+        console.log('heading-2')
+        const content = targetElementTwo.innerText;
+        const dateInfo = itemTwoDate.innerText;
+        displayContainer.innerText = '';
+        displayDateContainer.innerText = '';
+        displayContainer.innerText = content;
+        displayDateContainer.innerText = itemTwoDate.innerText;
+      }
+    });
+  },
+  { rootMargin: `-${50}px 0px 0px 0px`, threshold: [1.0] }
+);
+
+observerTwo.observe(targetElementTwo);
+
+
+
+
+
+
+// const targetElement = document.getElementById("simple-list-item-3");
+// const observer = new IntersectionObserver(
+//   (entries) => {
+//     entries.forEach((entry) => {
+//       const { top } = entry.boundingClientRect;
+//       const viewportTop =
+//         window.innerHeight || document.documentElement.clientHeight;
+//       if (top <= 200 && top >= -entry.boundingClientRect.height) {
+//         console.log("Element is at the top of the display.");
+//         targetElement.style.backgroundColor = "lightgreen"; // Change color when at the top
+//       } else {
+//         targetElement.style.backgroundColor = "lightcoral"; // Revert color when not at the top
+//       }
+//     });
+//   },
+//   { rootMargin: `-${50}px 0px 0px 0px`, threshold: [1.0] }
+// );
+
+// observer.observe(targetElement);
